3fc1724b50d78454866a30fc328de65d99fb5296
Thuyen <phantrongthuyen9d@gmail.com>
2020-05-10 20:59:50 +0700

    [sparrow] Init sparrow project

---
+++ SparrowProject/src/main/java/vn/sparrow/spring/SparrowProjectApplication.java
@@ -0,0 +1,13 @@
+package vn.sparrow.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparrowProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparrowProjectApplication.class, args);
+	}
+
+}
---
+++ SparrowProject/src/test/java/vn/sparrow/spring/SparrowProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.sparrow.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparrowProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
